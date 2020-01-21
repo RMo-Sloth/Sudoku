416248c93f4f4a0976ee14b65ac1326e27e987ae
Rob <robmonhemius@gmail.com>
2020-01-22 00:57:06 +0100

    facade

---
+++ utility/SudokuSolver/SudokuSolver.ts
@@ -0,0 +1,20 @@
+import { SquareCellSolver } from "./CellSolver/SquareSolver/SquareSolver";
+import { RowCellSolver } from "./CellSolver/RowSolver/RowCellSolver";
+import { ColumnCellSolver } from "./CellSolver/ColumnSolver/ColumnCellSolver";
+
+export class SudokuSolver { // facade
+  private _sudoku;
+
+  constructor( sudoku: string ) {
+    this._sudoku = sudoku;
+  }
+  columnSquareSolver() {
+    return new ColumnCellSolver(this._sudoku );
+  }
+  rowCellSolver() {
+    return new RowCellSolver( this._sudoku );
+  }
+  squareCellSolver( sudoku: string ) {
+      return new SquareCellSolver( this._sudoku );
+  }
+}
